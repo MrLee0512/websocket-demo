76cf2ebdf7cf358948f30f034b51e985f7396439
lizheng <lizheng0581@163.com>
2019-02-24 22:59:48 +0800

    完善

---
+++ src/main/java/com/liz/netty/Main.java
@@ -0,0 +1,33 @@
+package com.liz.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 程序的入口, 负责启动应用
+ * Created by liz on 2019/2/24 22:29
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.childHandler(new MyWebSocketChannelHandler());
+            System.out.println("服务端开启等待客户端连接...");
+            Channel ch = serverBootstrap.bind(8888).sync().channel();
+            ch.closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
--- src/main/java/com/liz/netty/MyWebSocketHandler.java
+++ src/main/java/com/liz/netty/MyWebSocketHandler.java
@@ -1,7 +1,19 @@
 package com.liz.netty;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.util.Date;
 
 /**
  * 接收/请求/响应客户端WebSocket请求的核心业务类
@@ -9,10 +21,11 @@ import io.netty.channel.SimpleChannelInboundHandler;
  */
 public class MyWebSocketHandler extends SimpleChannelInboundHandler<Object> {
 
+    private WebSocketServerHandshaker handshaker;
+    private static final String WEB_SOCKET_URL = "ws://localhost:8888/websocket";
+
     /**
      * 客户端与服务端创建连接的时候调用
-     * @param ctx
-     * @throws Exception
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -23,43 +36,105 @@ public class MyWebSocketHandler extends SimpleChannelInboundHandler<Object> {
 
     /**
      * 客户端与服务端断开连接的时候调用
-     * @param ctx
-     * @throws Exception
      */
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        super.channelInactive(ctx);
+        NettyConfig.group.remove(ctx.channel());
+        System.out.println("客户端与服务端连接关闭...");
     }
 
     /**
      * 服务端接收客户端发送过来的数据结束之后调用
-     * @param ctx
-     * @throws Exception
      */
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        super.channelReadComplete(ctx);
+        ctx.flush();
     }
 
     /**
      * 工程出现异常的时候调用
-     * @param ctx
-     * @param cause
-     * @throws Exception
      */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
     }
 
     /**
      * 服务端处理客户端WebSocket请求的核心方法
-     * @param ctx
-     * @param msg
-     * @throws Exception
      */
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 处理客户端向服务端发起的握手请求
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx, (FullHttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            // 处理websocket业务
+            handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
+    }
 
+    /**
+     * 处理websocket业务
+     */
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        // 如果是关闭websocket的指令
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), ((CloseWebSocketFrame) frame).retain());
+        }
+        // 如果是ping
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        // 如果是二进制消息
+        if (!(frame instanceof TextWebSocketFrame)) {
+            System.out.println("暂不支持二进制消息");
+            throw new RuntimeException("[" + this.getClass().getName() + "] 不支持消息");
+        }
+        // 获取文本消息
+        String text = ((TextWebSocketFrame) frame).text();
+        System.out.println("收到客户端消息 ====>> " + text);
+        // 群发应答
+        TextWebSocketFrame tws = new TextWebSocketFrame(new Date().toString() + ctx.channel().id() + " ====>> " + text);
+        NettyConfig.group.writeAndFlush(tws);
+    }
+
+    /**
+     * 处理客户端向服务端发起的握手请求
+     */
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        // 如果不成功或者请求头中的upgrade不是websocket
+        if (!req.getDecoderResult().isSuccess() || !"websocket".equals(req.headers().get("upgrade"))) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+        // 初始化WebSocketServerHandShaker
+        WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory(WEB_SOCKET_URL, null, false);
+        handshaker = factory.newHandshaker(req);
+        if (handshaker == null) {
+            // 表示不支持
+            WebSocketServerHandshakerFactory.sendUnsupportedWebSocketVersionResponse(ctx.channel());
+        } else {
+            // 握手
+            handshaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    /**
+     * 服务端向客户端发送http响应
+     */
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
+        // 如果不成功
+        if (res.getStatus().code() != 200) {
+            ByteBuf byteBuf = Unpooled.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(byteBuf);
+            byteBuf.release();
+        }
+        // 服务端向客户端发送数据
+        ChannelFuture channelFuture = ctx.channel().writeAndFlush(res);
+        if (res.getStatus().code() != 200) {
+            channelFuture.addListener(ChannelFutureListener.CLOSE);
+        }
     }
 }
